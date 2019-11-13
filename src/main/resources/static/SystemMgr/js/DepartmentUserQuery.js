410646c27fa2fb45fc93425855d49fc2f64e1b87
wangyc <409232112@qq.com>
2019-11-13 17:42:11 +0800

    add roleuser deptuser

---
+++ src/main/resources/static/SystemMgr/js/DepartmentUserQuery.js
@@ -0,0 +1,70 @@
+﻿$(function(){
+    //查询
+    $("#btn_query").click(function(){
+        query()
+    });
+
+    //清空表单
+    $("#btn_reset").click(function(){
+        reset()
+    });
+
+    //刷新表格
+    $("#btn_refresh").click(function(){
+        refresh()
+    });
+
+    //回车查询
+    $('#name').textbox('textbox').keydown(function (e) {
+        if (e.keyCode == 13) {
+            query();
+        }
+    });
+    $('#code').textbox('textbox').keydown(function (e) {
+        if (e.keyCode == 13) {
+            query();
+        }
+    });
+
+    $("#department_gridtree").treegrid({
+        onClickRow:function(data){
+            onClickRow(data);
+        }
+    });
+
+    $("#user_grid").datagrid({});
+    $("#user_grid").datagrid("options").url="user/find"
+
+})
+
+
+function query(){
+    var data =  form.getFormValues("department_form");
+    treeGrid.reloadWithData("department_gridtree",data)
+}
+
+function reset(){
+    form.resetForm("department_form")
+}
+
+function refresh(){
+    dialog.refresh()
+}
+
+
+function onClickRow(row){
+    var data={}
+    data["departmentId"] = row.id;
+    grid.reloadWithData("user_grid",data);
+}
+function searchData(value){
+    var row = grid.getCurrentSelectRowData("department_gridtree");
+    var data={}
+    data["departmentId"] = row.id;
+    data["name"] = value
+    grid.reloadWithData("user_grid",data);
+}
+
+
+
+
---
+++ src/main/resources/static/SystemMgr/js/RoleUserQuery.js
@@ -0,0 +1,63 @@
+﻿$(function(){
+    //查询
+    $("#btn_query").click(function(){
+        query()
+    });
+
+    //清空表单
+    $("#btn_reset").click(function(){
+        reset()
+    });
+
+    //刷新表格
+    $("#btn_refresh").click(function(){
+        refresh()
+    });
+
+    //回车查询
+    $('#name').textbox('textbox').keydown(function (e) {
+        if (e.keyCode == 13) {
+            query();
+        }
+    });
+
+    $("#role_grid").datagrid({
+        onClickRow:function(index,data){
+            onClickRow(data);
+        }
+    });
+
+    $("#user_grid").datagrid({});
+    $("#user_grid").datagrid("options").url="user/find"
+
+})
+
+
+function query(){
+    var data =  form.getFormValues("role_form");
+    grid.reloadWithData("role_grid",data)
+}
+
+function reset(){
+    form.resetForm("role_form")
+}
+
+
+function refresh(){
+    dialog.refresh()
+}
+
+function onClickRow(row){
+    var data={}
+    data["roleId"] = row.id;
+    grid.reloadWithData("user_grid",data);
+}
+function searchData(value){
+    var row = grid.getCurrentSelectRowData("role_grid");
+    var data={}
+    data["roleId"] = row.id;
+    data["name"] = value
+    grid.reloadWithData("user_grid",data);
+}
+
+
