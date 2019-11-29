3c412f0c6ea68af34f3213d50b1f812e63ca65f3
wangyc <409232112@qq.com>
2019-11-29 16:22:47 +0800

    update

--- src/main/java/com/wyc/systemmgr/controller/UrlController.java
+++ src/main/java/com/wyc/systemmgr/controller/UrlController.java
@@ -4,8 +4,11 @@ import com.wyc.core.base.exception.BaseException;
 import org.apache.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by wangyc on 2019/10/31.
@@ -34,12 +37,14 @@ public class UrlController {
     }
 
     @GetMapping("/403")
-    public String forbidden() throws BaseException {
+    public String forbidden(HttpServletResponse response) throws BaseException {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
         return "403.html";
     }
 
     @GetMapping("/404")
-    public String notFound() throws BaseException {
+    public String notFound(HttpServletResponse response) throws BaseException {
+        response.setStatus(HttpStatus.NOT_FOUND.value());
         return "404.html";
     }
 }
